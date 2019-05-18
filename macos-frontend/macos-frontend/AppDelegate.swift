02e5339473cba42d10400ef1f416625c38578eb8
Paul Colusso <paulcolusso@gmail.com>
2019-05-18 22:19:30 +1000

    started macos agent impl, added ffi harness in py

--- macos-frontend/macos-frontend/AppDelegate.swift
+++ macos-frontend/macos-frontend/AppDelegate.swift
@@ -11,16 +11,33 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("globe"))
+        }
+        
+        constructMenu()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    @objc func refreshNow(_ sender: Any?) {
+        // TODO: Implement.
+    }
+    
+    func constructMenu() {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Refresh Now", action: #selector(AppDelegate.refreshNow(_:)), keyEquivalent: "P"))
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+    }
 }
 
