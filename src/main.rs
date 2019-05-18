8659cf4d528f3a9681156b166486c353047a3484
Paul Colusso <paulcolusso@gmail.com>
2019-05-18 10:45:26 +1000

    refactored download code

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,11 @@ use serde::{Deserialize};
 use std::error::Error;
 use chrono::DateTime;
 use image::{DynamicImage, GenericImage, ImageBuffer, ImageFormat, RgbaImage};
-use std::fs::File;
-use std::io::Write;
 
 
 fn main() {
-    get_images().unwrap();
+    let planet = assemble(4);
+    planet.unwrap().save("out.png");
 }
 
 #[derive(Deserialize, Debug)]
@@ -16,54 +15,53 @@ struct LatestPayload {
     file: String
 }
 
-fn get_images() -> Result<(), Box<Error>> {
+//TODO: Add retries here.
+fn get_tile(time: DateTime<chrono::offset::FixedOffset>, size: u32, level: u32, x: u32, y: u32) -> Result<DynamicImage, Box<Error>> {
+    let base_url = format!("http://himawari8.nict.go.jp/img/D531106/{}d/{}/{}_{}_{}.png", level, size, time.format("%Y/%m/%d/%H%M%S"), x, y);
+    let mut response = reqwest::get(&base_url)?;
+    let mut buf: Vec<u8> = vec![];
+    response.copy_to(&mut buf)?;
+    let image = image::load_from_memory_with_format(&buf, ImageFormat::PNG)?;
+    Ok(image)
+}
+
+fn assemble(level: u32) -> Result<RgbaImage, Box<Error>> {
     let mut query_latest = reqwest::get("http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json")?;
     let payload: LatestPayload = query_latest.json()?;
-    let time_with_tz = format!("2019-05-17 00:50:00 +1000");
+    let time_with_tz = format!("{} +1000", payload.date);
     let time = DateTime::parse_from_str(&time_with_tz, "%Y-%m-%d %H:%M:%S %z")?;
-
-    let WIDTH = 550;
-    let HEIGHT = 550;
-    let level = 4; // increases the quality (and the size) of each tile. possible values are 4, 8, 16, 20
+    let tile_width = 550;
+    let tile_height = 550;
+    let render_width = level * tile_width;
+    let render_height = level * tile_height;
+    //TODO: Verify level is one of the following 4, 8, 16, 20
 
     let mut tiles: Vec<DynamicImage> = vec![];
+    let mut render: RgbaImage = ImageBuffer::new(render_width, render_height);
 
     // TODO: mmmmultithread
     for x in 0..level {
         for y in 0..level {
             //TODO: retries
-            let base_url = format!("http://himawari8.nict.go.jp/img/D531106/{}d/{}/{}_{}_{}.png", level, WIDTH, time.format("%Y/%m/%d/%H%M%S"), x, y);
-            let mut response = reqwest::get(&base_url)?;
-            let mut buf: Vec<u8> = vec![];
-            response.copy_to(&mut buf)?;
-            
-            let image = image::load_from_memory_with_format(&buf, ImageFormat::PNG)?;
-            tiles.push(image);
+            let image = get_tile(time, tile_width, level, x, y);
+            tiles.push(image.unwrap());
             println!("Downloaded image for {},{}", x, y);
         }
     }
 
     tiles.reverse();
 
-    //TODO: y'know good code practices...
-    let wallpaper_width = 2200;
-    let wallpaper_height = 2200;
-    //FUCKING MATHS STOOPID BRAIN CMON A 9YO CAN DO THIS
-    let y_offset = 0; //wallpaper_height - HEIGHT * level;
-    let x_offset = 0;// wallpaper_width - WIDTH * level;
-
-    let mut wallpaper: RgbaImage = ImageBuffer::new(wallpaper_width, wallpaper_height);
+    //TODO: Could this be done without the vec?
+    
 
     for x in 0..level {
         for y in 0..level {
-            let x_pixel = x_offset + x * WIDTH;
-            let y_pixel = y_offset + y * HEIGHT;
+            let x_pixel = x * tile_width;
+            let y_pixel = y * tile_height;
             println!("Copying for {},{}. Starting from {},{}.", x, y, x_pixel, y_pixel);
-            wallpaper.copy_from(&tiles.pop().unwrap(), x_pixel, y_pixel );
+            render.copy_from(&tiles.pop().unwrap(), x_pixel, y_pixel );
         }
     }
 
-    wallpaper.save("yayayayayaya.png")?;
-
-    Ok(())
+    Ok(render)
 }
