3fd65b5ffd25bc947647b8d97e7beeb5cd2dd010
Paul Colusso <paulcolusso@gmail.com>
2019-05-18 17:13:28 +1000

    add mac ui

---
+++ macos-frontend/macos-frontend/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  macos-frontend
+//
+//  Created by Paul Colusso on 18/5/19.
+//  Copyright © 2019 Paul Colusso. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "himawari_render.h"
+
+#endif /* Bridging_Header_h */
