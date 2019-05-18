0ba58c768216302e1922e76cac8a2201117248d4
Paul Colusso <paulcolusso@gmail.com>
2019-05-18 23:56:11 +1000

    added windows frontend

---
+++ windows-frontend/windows-frontend/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace windows_frontend
+{
+  static class Program
+  {
+    [DllImport("himawari_render.dll")]
+    private static extern String save_planet(String path, Int32 level);
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+
+      save_planet("out.png", 4);
+    }
+  }
+}
