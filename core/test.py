4372fe0eae65bdbcf91a94092bd4c75629822297
Paul Colusso <paulcolusso@gmail.com>
2019-05-19 14:02:04 +1000

    fixed string return value

--- core/test.py
+++ core/test.py
@@ -1,11 +1,14 @@
-import ctypes
+import ctypes, platform
 
-# This will need to be altered per-platform
-lib = ctypes.cdll.LoadLibrary('target/release/libhimawari_render.dylib')
+lib_names = {
+    "Windows": "himawari_render.dll",
+    "macOS": "libhimawari_render.dylib"
+}
+lib = ctypes.cdll.LoadLibrary('target/release/' + lib_names.get(platform.system()))
 
 lib.save_planet.restype = ctypes.c_char_p
 lib.save_planet.argtypes = (ctypes.c_char_p, ctypes.c_uint)
 
 result = lib.save_planet(b'py.png', 8)
 
-print(result.value)
+print(result)
