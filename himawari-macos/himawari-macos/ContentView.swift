67cdd999f77c7feb065b6178706b68fb01c005ed
Paul Colusso <paulcolusso@gmail.com>
2022-01-30 13:48:30 +1100

    work on macos version

--- himawari-macos/himawari-macos/ContentView.swift
+++ himawari-macos/himawari-macos/ContentView.swift
@@ -6,13 +6,51 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
-    @Binding public var manager: WallpaperConfig
+    @EnvironmentObject var manager: WallpaperConfig
+    @State var quality = "2"
+    @State var width = "3840"
+    @State var height = "2160"
     
     var body: some View {
-        Button("Do the thing") {
-            manager.generate_wallpaper()
-        }
+        VStack(alignment: .center, spacing: 10.0){
+            Form {
+                TextField("Width of the generated wallpaper: ", text: $width)
+                TextField("Height of the generated wallpaper: ", text: Binding<String>(
+                    get: {
+                        "poop"
+                    },
+                    set: { newValue in
+                        
+                    }
+                ))
+                    .onReceive(Just(width)) { newValue in
+                        let filtered = newValue.filter { "0123456789".contains($0) }
+                        if filtered != newValue {
+                            self.quality = filtered
+                        }
+                    }
+                TextField("Quality Level: ", text: $quality)
+                    .onReceive(Just(quality)) { newValue in
+                        let filtered = newValue.filter { "1234".contains($0) }
+                        if filtered != newValue {
+                            self.quality = filtered
+                        }
+                    }
+                Button("Manually Refresh Now") {
+                    DispatchQueue.global(qos: .background).async {
+                        manager.generate_wallpaper()
+                    }
+                }
+            }
+        }.frame(width: 600, height: 160, alignment: .center)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
